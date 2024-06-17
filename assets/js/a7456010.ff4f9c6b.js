d1141ccab1e743e1448b8543310ea631689c4646
hassanzohdy <hassanzohdy@gmail.com>
2024-06-17 13:11:05 +0300

    'update'

---
+++ assets/js/a7456010.ff4f9c6b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmongez_docs=self.webpackChunkmongez_docs||[]).push([[1235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ assets/js/aba21aa0.ab64bcaa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmongez_docs=self.webpackChunkmongez_docs||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
